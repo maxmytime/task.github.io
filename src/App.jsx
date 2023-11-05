75f5c22bb1d96ef5bc4983c18ba5f60d5ac5ca03
Maksim <maxmytime@yandex.ru>
2023-11-05 19:10:26 +0700

    Start project

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+// import { React, useState } from 'react'
+import { useState } from 'react';
+import Task from './Task.jsx'
+import './main.css'
+
+function App() {
+    const [tasks, updateTaskSheet] = useState([
+        {content: 'Купить хлеб', completed: true, id: '1'},
+        {content: 'Купить хлеб1', completed: false, id: '2'}
+    ]);
+
+    function genID() {
+        const min = 10;
+        const max = 10000000;
+        return String(Math.round(Math.random() * (max - min) + min));
+    }
+
+    function addTask(e) {
+        e.preventDefault();
+        updateTaskSheet([
+            ...tasks, {content: '', completed: false, id: genID()}
+        ]);
+    }
+
+    function delTask(id) {
+        updateTaskSheet(tasks.filter((task) => task.id != id));
+    }
+
+    function setTaskStatus(completed, id) {
+        updateTaskSheet(tasks.map(task => task.id != id ? task : {
+            ...task, completed: !completed
+        }));
+	}
+
+    let tasksList = tasks.map((task) => <Task
+                                            content={task.content}
+                                            completed={task.completed}
+                                            id={task.id}
+                                            key={task.id}
+                                            delTask={delTask}
+                                            setTaskStatus={setTaskStatus}
+                                        />);
+
+    return (
+        <>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-6">
+                        <h1 className="title title-h1 text-center">Мои задачи</h1>
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-6">
+                        <div className="task-list js-task-list">
+                            {tasksList}
+                        </div>
+                    </div>
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-6 text-right js-btn-add-task">
+                        <a href="#" className="button js-button-add-task" onClick={addTask}>+ Задача</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default App
